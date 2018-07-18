11fbfce8d7b7e55dc852ec08c69c755edbdd72b2
catdolls <baby_of_wind@hotmail.com>
2018-07-19 01:53:13 +0700

    set color

--- FullCalendar/FullCalendar/ClientApp/src/app/app.component.ts
+++ FullCalendar/FullCalendar/ClientApp/src/app/app.component.ts
@@ -9,10 +9,10 @@ import { EventService } from './modules/cores/services';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  title = 'Calendar';
   calendarOptions: Options;
+  displayEvent: any;
   @ViewChild(CalendarComponent) ucCalendar: CalendarComponent;
-  title = 'app';
-
   constructor(protected eventService: EventService) { }
 
   ngOnInit() {
@@ -28,5 +28,38 @@ export class AppComponent implements OnInit {
         events: data
       };
     });
+  }
+  clickButton(model: any) {
+    this.displayEvent = model;
+  }
+  eventClick(model: any) {
+    console.log('modal');
+    model = {
+      event: {
+        id: model.event.id,
+        start: model.event.start,
+        end: model.event.end,
+        title: model.event.title,
+        allDay: model.event.allDay
+        // other params
+      },
+      duration: {}
+    }
+    this.displayEvent = model;
+  }
+  updateEvent(model: any) {
+    model = {
+      event: {
+        id: model.event.id,
+        start: model.event.start,
+        end: model.event.end,
+        title: model.event.title
+        // other params
+      },
+      duration: {
+        _data: model.duration._data
+      }
+    }
+    this.displayEvent = model;
   }
 }
